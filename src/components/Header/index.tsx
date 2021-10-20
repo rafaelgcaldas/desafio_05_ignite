6cacb6039d97c0fe17bb9465dfbbb15440ae3e25
Rafael <rafael.gcaldas01@gmail.com>
2021-10-19 22:10:07 -0300

    Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import styles from "./header.module.scss";
+
+export function Header() {
+  return (
+    <header>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <img src="/logo.svg" alt="logo" />
+        </div>
+      </div>
+    </header>
+  )
 }
